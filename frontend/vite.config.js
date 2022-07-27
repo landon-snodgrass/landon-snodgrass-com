c842cc4bc99f765ef37bd4eceee811e377c0a51f
Landon Snodgrass <g.l.snodgrass95@gmail.com>
2022-07-26 22:22:28 -0700

    blah

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig(({ command }) => {
                 },
             },
             root: './',
+            base: '/frontend/',
             build: {
                 outDir: '../dist',
                 rollupOptions: {
